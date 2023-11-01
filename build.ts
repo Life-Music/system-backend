15b033128816dbe06fc7b52e633d65783b258dab
Ily1606 <khuonmatdangthuong45@gmail.com>
2023-11-01 10:29:13 +0700

    fix: delete view command

--- build.ts
+++ build.ts
@@ -16,7 +16,6 @@ import childProcess from 'child_process';
     await remove('./dist/');
     // Copy front-end files
     await copy('./src/public', './dist/public');
-    await copy('./src/views', './dist/views');
     // Copy back-end files
     await exec('tsc --build tsconfig.prod.json', './');
   } catch (err) {
@@ -52,7 +51,7 @@ function copy(src: string, dest: string): Promise<void> {
  */
 function exec(cmd: string, loc: string): Promise<void> {
   return new Promise((res, rej) => {
-    return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
+    return childProcess.exec(cmd, { cwd: loc }, (err, stdout, stderr) => {
       if (!!stdout) {
         logger.info(stdout);
       }
